aa177ed07ebfb015736f8b14b7f6856d5ece7304
woland7 <antonio.deiasio@gmail.com>
2016-07-31 01:33:15 +0200

    First version UI

---
+++ src/main/java/controller/UIController.java
@@ -0,0 +1,18 @@
+package controller;
+
+import javafx.fxml.FXML;
+
+/**
+ * Created by Antonio on 31/07/2016.
+ */
+public class UIController {
+
+    public UIController(){
+
+    }
+
+    @FXML
+    private void initialize(){
+
+    }
+}
