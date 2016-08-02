1c66631ffe65037763f993c2d95f1d97ee7faeb6
woland7 <antonio.deiasio@gmail.com>
2016-08-02 13:08:40 +0200

    Added file chooser open dialog.

--- src/main/java/analizzatore/prototipo/controller/UIController.java
+++ src/main/java/analizzatore/prototipo/controller/UIController.java
@@ -3,6 +3,10 @@ package analizzatore.prototipo.controller;
 import analizzatore.prototipo.AnalizzatoreUI;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
 
 
 /**
@@ -16,6 +20,8 @@ public class UIController {
 
     private AnalizzatoreUI aui;
 
+    private File file = null;
+
     public UIController(){
 
     }
@@ -28,7 +34,12 @@ public class UIController {
 
     @FXML
     private void handleNuovo(){
-
+        FileChooser fC = new FileChooser();
+        fC.setTitle("Scegli capture file da analizzare");
+        FileChooser.ExtensionFilter ef = new FileChooser.ExtensionFilter("CSV", "*.csv");
+        fC.setInitialDirectory(new File(System.getProperty("user.home")));
+        fC.getExtensionFilters().add(ef);
+        file = fC.showOpenDialog(new Stage());
     }
 
     public void setAnalizzatoreUI(AnalizzatoreUI aui){
